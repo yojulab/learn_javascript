278a37d477606f361207a415a62596bbf8f8ac03
Sanghun Oh <otter.oh@gmail.com>
2022-06-23 18:29:11 +0900

    add some functions

--- codes/functions/functions.js
+++ codes/functions/functions.js
@@ -1,31 +1,40 @@
-const item_1 = 1000;
-const item_2 = 2000;
-const item_3 = 3000;
+const number_1 = 1000;
+const number_2 = 2000;
+const number_3 = 3000;
 
-// 호이스팅(hoisting), 
+// basic function, 호이스팅(hoisting) is support,
 // function to calculate the total price
-console.log(calculateSum1(item_1, item_2, item_3));
+console.log(calculateSum1(number_1, number_2, number_3));
 function calculateSum1(item_1, item_2, item_3) {
   return item_1 + item_2 + item_3;
 }
+// console.log(calculateSum1(number_1, number_2, number_3));
 
-// console.log(calculateSum2(item_1, item_2, item_3)); // Error
-const calculateSum2 = (item_1, item_2, item_3) => {
+// anonymous function, hoisting not support,
+// console.log(calculateSum2(number_1, number_2, number_3)); // Error
+const calculateSum2 = function (item_1, item_2, item_3) {
   return item_1 + item_2 + item_3;
-}
-console.log(calculateSum2(item_1, item_2, item_3));
+};
+console.log(calculateSum2(number_1, number_2, number_3));
+
+// arrow function, hoisting not support, from EC6
+// console.log(calculateSum3(number_1, number_2, number_3)); // Error
+const calculateSum3 = (item_1, item_2, item_3) => {
+  return item_1 + item_2 + item_3;
+};
+console.log(calculateSum3(number_1, number_2, number_3));
 
 // 렉시컬 스코프(Lexical Scope)
 const lexical_scope = {
-    name: 'Lexical Scope',
-    sayName1: function() {
-        // console.log(this.name);
-        console.log(this);
-    },
-    sayName2: () => {
-        // console.log(this.name);
-        console.log(this);
-    }
+  name: "Lexical Scope",
+  sayName1: function () {
+    // console.log(this.name);
+    console.log(this);
+  },
+  sayName2: () => {
+    // console.log(this.name);
+    console.log(this);
+  },
 };
 
 // console.log(lexical_scope.sayName1());
--- codes/variables/objects.js
+++ codes/variables/objects.js
@@ -1,11 +1,19 @@
+// define literal way of OOP
 const monkey = {
-    name: 'George',
-    species: 'gorilla',
-    foodsEaten: ['bananas', 'apples', 'pears'],
-    eatSomething: function(food) {
-        this.foodsEaten.push(food);
-    },
+  name: "George",
+  species: "gorilla",
+  foodsEaten: ["bananas", "apples", "pears"],
+  eatSomething: function (food) {
+    this.foodsEaten.push(food);
+    console.log(this); // this --> monkey obj
+  },
+  eatSomethingArrow: (food) => {
+    console.log(this); // this --> global
+    this.foodsEaten.push(food); // Error
+  },
 };
-monkey.name = 'John';
+monkey.name = "John";
 console.log(monkey);
 console.log(monkey.foodsEaten);
+monkey.eatSomething("mangos");
+monkey.eatSomethingArrow("strawberry");
