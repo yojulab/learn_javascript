f81479be02d3e532f4d4f7136b423be842eb5c39
Sanghun Oh <otter.oh@gmail.com>
2022-07-01 00:00:21 +0900

    add some refer

--- codes/DOMs/events.js
+++ codes/DOMs/events.js
@@ -1,14 +1,16 @@
-'use strict';
+"use strict";
 
-const btn = document.querySelector('#btn');
+const btn = document.querySelector("#btn");
 
-btn.addEventListener('click', () => {
-  console.log('Button Clicked');
+btn.addEventListener("click", () => {
+  console.log("Button Clicked");
 });
 
-const google = document.querySelector('#google');
-google.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log('Google Clicked');
-    });
-    
+const google = document.querySelector("#google");
+google.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log("Google Clicked");
+});
+
+// 이벤트 위임
+// refer : https://www.notion.so/Javascript-Window-DOM-BOM-a0d3b424fc4c48559a1827c303b2f903
