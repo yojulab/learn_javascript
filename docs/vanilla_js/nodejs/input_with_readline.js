56991b0e37f34ecab84b83098ace8837bd600b61
Sanghun Oh <otter.oh@gmail.com>
2022-11-16 19:22:25 +0900

    add input with readline

---
+++ docs/vanilla_js/nodejs/input_with_readline.js
@@ -0,0 +1,10 @@
+// Not appearing int debug Mode
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question("Who are you?", (name) => {
+  console.log(`Hey there ${name}!`);
+  readline.close();
+});
