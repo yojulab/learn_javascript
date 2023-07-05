f5db783ba6728482bb5512e60f7162a582663f46
Sanghun Oh <otter.oh@gmail.com>
2023-07-05 12:28:06 +0900

    Update fetchs.js

--- docs/vanilla_js/apis/fetchs.js
+++ docs/vanilla_js/apis/fetchs.js
@@ -1,8 +1,12 @@
 'use strict';
-
+# refer : https://www.daleseo.com/js-window-fetch/
 const url = 'https://api.covid19api.com/country/south-africa/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z';
 
-const request = fetch(url).then(response => response.json())
-    .then(data => {
+const request = fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+        console.log(data);
+    })
+    .catch(data => {
         console.log(data);
     });
